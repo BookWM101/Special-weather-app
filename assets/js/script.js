ae4d253b2dcbec4110ffef7eb95df7d277a0e4c7
Kayla Flint <bookwm2016@gmail.com>
2024-05-08 20:37:22 -0400

    added js for change in weather symbol

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const tempBtn = document.getElementById('fahrenheit/celsius');
+let symbol = document.getElementById('weatherSymbol');
+let weather = fetch() //fetch current weather from api//
 let temp = cel;
-let cel = fetch();
+let cel = fetch(); //fetch current degrees in celsius from api//
 let fah = (cel*9.0/5.0)+32.0;
 
 function tempChange () {
@@ -9,6 +11,24 @@ function tempChange () {
         else {
             temp = cel
         }
+        return
 }
 
 tempBtn.addEventListener('click', tempChange);
+
+function changeSymbol () {
+if (temp < 32) {
+    symbol.attributes('src') == "./assets/images.snow-flake.webp";
+    }
+    else if (weather = sunny) {
+        symbol.attributes('src') == "./assets/images.sunny.jpg";
+    }
+    else if (weather = stormy) {
+        symbol.attributes('src') == "./assets/images.storms.jpg";
+    }
+    else if (weather = cloudy) {
+        symbol.attributes('src') == "./assets/images.cloudy.png";
+    }
+    return
+}
+
